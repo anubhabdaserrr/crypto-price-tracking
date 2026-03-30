991732024346a92ff55b679ec1b200e857e3a7fe
Anubhab Das <55646192+anubhabdaserrr@users.noreply.github.com>
2026-03-31 02:08:44 +0530

    client.py updated

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import websockets
 import asyncio
 
 async def test():
-    uri = "ws://localhost:8000/ws"
+    uri = "wss://crypto-price-tracking-production.up.railway.app/ws" #"ws://localhost:8000/ws"
 
     async with websockets.connect(uri) as ws:
         while True:
